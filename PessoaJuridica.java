6f489a611f0a892c6d1b9a8b92552c2e3d0838db
Gabriela Ramire <gblove16@gmail.com>
2019-03-25 19:49:54 -0300

    Adicionando pessoa fisica e pessoa juridica

---
+++ PessoaJuridica.java
@@ -0,0 +1,18 @@
+public class PessoaJuridica extends Cliente {
+    private int cnpj;
+
+    public PessoaJuridica() {
+
+    }
+
+    public PessoaJuridica(int cnpj) {
+        this.cnpj = 
+    }
+
+    public void setCNPJ(int cnpj) {
+        this.cnpj = cnpj;
+    }
+    public int getCNPJ() {
+        return this.cnpj;
+    }
+}
