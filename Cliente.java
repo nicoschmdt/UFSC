beec5f6ac0edf0c04327f2b5fe56db01bc15d319
Gabriela Ramire <gblove16@gmail.com>
2019-03-25 19:41:29 -0300

    Adicionando cliente e Agencia

---
+++ Cliente.java
@@ -0,0 +1,50 @@
+
+public class Cliente {
+	private String nome;
+	private String endereco;
+	private String telefone; 
+	private String email;
+	private Conta[] contas;
+	
+	public Cliente() {
+		
+	}
+	public Cliente(String nome, String endereco, String telefone, String email, Conta[] contas) {
+		this.nome=nome;
+		this.endereco=endereco;
+		this.telefone=telefone;
+		this.email=email;
+		this.contas=contas;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Conta[] getContas() {
+		return contas;
+	}
+	public void setContas(Conta[] contas) {
+		this.contas = contas;
+	}
+	
+}
