462f2d23eb79c00c531d7b8990dd0d2d4695367f
Nicole Schmidt <nicoleschmidt2000@gmail.com>
2019-06-16 19:53:08 -0300

    Added new variables and get/set methods

--- Field.java
+++ Field.java
@@ -2,15 +2,15 @@
 public class Field {
 	//this class is for an only camp
 	//preciso implementar nessa classe o flood system
-	//o bomb placement
 	//how the click thingy works, theres the 3 ifs I have to do
 	//gotta do the lose system too, maybe in another class?
-	//i think I can merge de Camp class and the Map class
 	
 	private boolean have_bomb;
+	private boolean have_number;
+	private int number;
 	
 	public Field() {
-		
+		have_bomb = false;
 	}
 	
 	public void set_bomb() {//esse set funciona..?
@@ -18,6 +18,18 @@ public class Field {
 	}
 	public boolean have_bomb() {
 		return have_bomb;
+	}
+	public void set_number() {
+		have_number = true;
+	}
+	public boolean have_number() {
+		return have_number;
+	}
+	public void set_number(int n) {
+		number = n;
+	}
+	public int get_number() {
+		return number;
 	}
 	//method that returns how much bombs are near the area clicked
 	public int bombs_near() {
