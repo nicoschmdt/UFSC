62a8c34ff4dc286c6c7154d1f0eb8b490060c6bd
Nicole Schmidt <nicoleschmidt2000@gmail.com>
2019-06-16 19:58:49 -0300

    restart button and some JLabels added.

--- Interface.java
+++ Interface.java
@@ -1,38 +1,84 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
+import javax.swing.SwingConstants;
 
-public class Interface extends JFrame{
+public class Interface {
 	private String[] levels = {"Easy","Medium","Hard"}; 
 	private JMenu file_Menu;
 	private JMenuItem about;
 	private JMenu difficult;
 	private JMenuBar bar;
+	private JFrame frame;
 	private JRadioButtonMenuItem[] level; // think of a better name
 	private ButtonGroup levelButtonGroup;
 	private Map map;
 	// I believe I have to add a JPanel(?)
-	private JPanel panel;
+	private JPanel panel,panel_two;
 	//trying to add the select level
 	private String selected_difficult = "Easy";
+	//
+	private JLabel label_bombs,label_timer;
+	//
+	private JButton restart_b;
+	private Box box;
 	
 	public Interface() {
-		super("Minesweeper");
 		//PANEL
-		setLayout(new FlowLayout());
+
+		frame = new JFrame("Minesweeper");
+		frame.setLayout(new BorderLayout());
 		
-		panel = new JPanel(new BorderLayout());
-		panel.setPreferredSize(new Dimension(600,600));
+		panel = new JPanel(new FlowLayout());
+		panel.setPreferredSize(new Dimension(250,200));
+		panel.setBackground(Color.DARK_GRAY);
+		//test 1506 I'm trying to add the timer panel and the bombs panel but idk how to do this
+		//I managed to add another panel but still it remained as a wip
+		panel_two = new JPanel();
+		
+		panel_two.setPreferredSize(new Dimension(50,50));
+		panel_two.setBackground(Color.YELLOW);
+		panel_two.setLayout(new BoxLayout(panel_two,BoxLayout.X_AXIS));
+		panel_two.setBorder(BorderFactory.createEmptyBorder(0,10,10,10));
+		//Label test
+		//it'll have to say how many undentified bombs are in the map
+		//gotta make a method for that
+		//box
+		box = Box.createHorizontalBox();
+		//
+		label_bombs = new JLabel();
+		label_bombs.setText("Bombs remaining: ");
+		label_timer = new JLabel();
+		label_timer.setText("Time Passed: ");
+		
+		box.add(label_bombs);
+		box.add(Box.createHorizontalStrut(110));
+		//gotta fix the size, cant really set this in i3 :/
+		restart_b = new JButton("Restart");
+		restart_b.setPreferredSize(new Dimension(40,40));
+		box.add(restart_b);
+		box.add(Box.createHorizontalStrut(110));
+		//
+		box.add(label_timer);
+		panel_two.add(box);
 		//
 		
 		file_Menu = new JMenu("File");
@@ -62,19 +108,29 @@ public class Interface extends JFrame{
 		//the about actionListener
 		about.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(Interface.this,
+				JOptionPane.showMessageDialog(null,
 						"Hello!\n\nIf you have never played Minesweeper before here is what you have to do!\nTry clicking the buttons bellow, you'll see that it appears a number\nthis number show the quantity of bombs near the place you just clicked\nyour objective is to clear all the area that doesn't have bombs!\n\n Good luck!",
 						"About",JOptionPane.PLAIN_MESSAGE);
+				
 			}
 		});
 		bar = new JMenuBar();
-		setJMenuBar(bar);
 		bar.add(file_Menu);
 		
 		//map
 		map = new Map();
-		panel.add(map);
-		super.add(panel);
+		panel.add(map,BorderLayout.CENTER);
+		
+		//frame add
+		frame.setJMenuBar(bar);
+		frame.add(panel_two,BorderLayout.PAGE_START);
+		frame.add(panel,BorderLayout.CENTER);
+		
+		//basic
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600,400);
+		frame.setVisible(true);
+
 		
 		
 	}
@@ -88,8 +144,5 @@ public class Interface extends JFrame{
 	//main
 	public static void main(String[] args) {
 		Interface window = new Interface();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(1000,1000);
-		window.setVisible(true);
 	}
 }
