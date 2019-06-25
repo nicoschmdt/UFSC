4559898c1dfe2aa51dec1582188e06da422de661
Nico Schmidt <nicoleschmidt2000@gmail.com>
2019-06-25 11:00:49 -0300

    Added new gets/sets

--- Field.java
+++ Field.java
@@ -8,28 +8,44 @@ public class Field {
 	private boolean have_bomb;
 	private boolean have_number;
 	private int number;
+	private boolean have_flag;
+	private boolean is_clicked;
 	
 	public Field() {
 		have_bomb = false;
+		have_flag = false;
+		is_clicked = false;
 	}
 	
 	public void set_bomb() {//esse set funciona..?
-		have_bomb = true;		
+		this.have_bomb = true;	
 	}
 	public boolean have_bomb() {
-		return have_bomb;
+		return this.have_bomb;
 	}
 	public void set_number() {
-		have_number = true;
+		this.have_number = true;
 	}
 	public boolean have_number() {
-		return have_number;
+		return this.have_number;
 	}
 	public void set_number(int n) {
-		number = n;
+		this.number = n;
 	}
 	public int get_number() {
-		return number;
+		return this.number;
+	}
+	public void set_have_flag(boolean f) {
+		this.have_flag = f;
+	}
+	public boolean get_flag() {
+		return this.have_flag;
+	}
+	public void set_click() {
+		this.is_clicked = true;
+	}
+	public boolean get_click() {
+		return this.is_clicked;
 	}
 	//method that returns how much bombs are near the area clicked
 	public int bombs_near() {
