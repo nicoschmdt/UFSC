f66b86e85bacadfcbf0ced8d3fc565fa268a58d8
Gagocampos <gabrielcampos3@hotmail.com>
2021-08-02 22:06:04 -0300

    metodo para inserir concatenação

---
+++ insertConcat.py
@@ -0,0 +1,15 @@
+import string
+
+palavra = '().+*|'
+
+def insertConcat(expr):
+    for i in range(len(expr)-1):
+        if expr[i] not in palavra and expr[i+1] not in palavra:
+            expr = expr[:i+1] + '.' + expr[i+1:]
+            return insertConcat(expr)
+        elif expr[i] == ')' and expr[i+1] not in palavra:
+            expr = expr[:i+1] + '.' + expr[i+1:]
+            return insertConcat(expr)
+    return expr
+
+print(insertConcat('(a|b)aab'))
