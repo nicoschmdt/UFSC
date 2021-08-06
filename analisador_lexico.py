0ffb6b990b9d1ef37728093dcc2d5ef6d74eb4f2
Nicole Schmidt <nicoleschmidt2000@gmail.com>
2021-08-06 14:08:42 -0300

    add ER_to_AFD method, move insert_concat to analisador_lexico.py

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -11,6 +11,23 @@ class TreeNode:
     last_pos: set = field(default_factory=set)
     follow_pos: set = field(default_factory=set)
 
+@dataclass
+class Automata:
+    inital_state: set
+    acceptance: set
+    transitions: dict[tuple[set, str], set]
+
+def insert_concat(expr):
+    palavra = '().+*|'
+    for i in range(len(expr)-1):
+        if expr[i] not in palavra and expr[i+1] not in palavra:
+            expr = expr[:i+1] + '.' + expr[i+1:]
+            return insert_concat(expr)
+        elif expr[i] not in '(|.' and expr[i+1] not in ')+*|.':
+            expr = expr[:i+1] + '.' + expr[i+1:]
+            return insert_concat(expr)
+    return expr
+
 def rpn(expr):
     binary_operators = ['|','.']
     output = []
@@ -34,7 +51,6 @@ def rpn(expr):
 
     return output
 
-# ['a','a','.','b','*','.','#','.']
 def tree(rpn_list):
     *head, symbol = rpn_list
     if symbol in ['.','|']:
@@ -97,8 +113,11 @@ def first_pos_last_pos_and_nullable(tree):
 
 def follow_pos(tree, lista):
     if tree.right_node == None and tree.left_node == None:
-        tree.follow_pos = lista
-    
+        if tree.value == '#':
+            tree.follow_pos = set()
+        else:
+            tree.follow_pos = lista
+
     elif tree.value in '*+':
         follow_pos(tree.left_node, tree.first_pos.union(lista))
 
@@ -110,6 +129,61 @@ def follow_pos(tree, lista):
         follow_pos(tree.left_node, lista)
         follow_pos(tree.right_node, lista)
 
+def get_follow_pos_table(tree):
+    table = {}
+    input_symbols = set()
+    stack = [tree]
+    while stack:
+        node = stack.pop()
+        if node.left_node != None and node.right_node != None:
+            stack.append(node.left_node)
+            stack.append(node.right_node)
+        elif node.right_node != None:
+            stack.append(right_node)
+        else:
+            input_symbols |= {node.value}
+            if node.value != '#':
+                table[node.first_pos.pop()] = (node.follow_pos,node.value)
+    return table, input_symbols
+
+def construct_AFD(tree,expr,number):
+    follow_pos_table, input_symbols = get_follow_pos_table(tree)
+    d_states = tree.first_pos
+    d_transitions = {}
+    symbols = set(follow_pos_table)
+
+    # not_marked = set(follow_pos_table.keys())
+    not_marked = tree.first_pos
+
+    while not_marked:
+        T = not_marked.pop()
+        for symbol in input_symbols:
+            U = set()
+            for state in T:
+                if state == follow_pos_table[state][1]:
+                    U |= follow_pos_table
+                if U and U not in d_states:
+                    not_marked.append(U)
+                d_transitions[(T,state)] = U
+
+    acceptance_list = set()
+    for state in d_states:
+        if number in state:
+            acceptance_list |= {state}
+
+    return Automata(
+        inital_state=tree.follow_pos,
+        acceptance=acceptance_list,
+        transitions=d_transitions,
+    )
+
+def ER_to_AFD(expr):
+    expr = insert_concat(expr+'#')
+    rpn = rpn(expr)
+    tree = tree(rpn)
+    last_leaf = enumerate_tree_leaf(tree) - 1
+    follow_pos(tree,set())
+    automata = construct_AFD(tree,expr,last_leaf)
 
 def tree_to_tuple(tree):
     if tree is None:
