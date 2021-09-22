f5409f25640d9bd21045fe4e3fa4c4e8659985bd
Nicole Schmidt <nicoleschmidt2000@gmail.com>
2021-09-22 02:17:09 -0300

    adapted direct indetermination removal to new token representation

--- analisador_sintatico.py
+++ analisador_sintatico.py
@@ -1,3 +1,5 @@
+import copy
+from typing import ClassVar
 
 def remove_left_recursion(grammar):
     new_grammar = {}
@@ -46,7 +48,156 @@ def remove_left_recursion(grammar):
                 new_grammar[name[1:]].append(new_syntax + [name])
             new_grammar[name[1:]].append(epsilon)
 
-
-    from pprint import pprint
-    pprint(new_grammar)
     return new_grammar
+
+# FATORAÇÃO E FIRST?
+
+# insere lista na chave se vazio, append se ja existe
+def dictInsertAppend(dicionarioDict, chave, valor):
+    if chave not in dicionarioDict:
+        dicionarioDict[chave] = [valor]
+    else:
+        dicionarioDict[chave].append(valor)
+    return dicionarioDict
+
+# insere lista na chave se vazio, append se ja existe nao adiciona valores repetidos a lista
+def dictExclusiveInsertAppend(dicionarioDict, chave, valor):
+    inserted = False
+    if chave not in dicionarioDict:
+        dicionarioDict[chave] = [valor]
+        inserted = True
+    else:
+        if valor not in dicionarioDict[chave]:
+            dicionarioDict[chave].append(valor)
+            inserted = True
+    return dicionarioDict, inserted;
+
+
+def direct_indeterminant_productions(grammar: dict):
+    sub_indeterminant_grammar = {}
+    for nonterminal, productions in grammar.items():
+        repetitions = {}
+        for production in productions:
+            head, *_ = production
+            try:
+                repetitions[head] += [production]
+            except KeyError:
+                repetitions[head] = [production]
+        print(f'repetitions: {repetitions}')
+        for head, productions in repetitions.items():
+            if len(productions) > 1:
+                try:
+                    sub_indeterminant_grammar[nonterminal] += productions
+                except KeyError:
+                    sub_indeterminant_grammar[nonterminal] = productions
+    return sub_indeterminant_grammar
+
+
+def remove_direct_indetermination(grammar: dict):
+    new_grammar = {}
+    producoes_indeterminantes = direct_indeterminant_productions(grammar)
+
+    for nonterminal, productions in grammar.items():
+        if (nonterminal, productions) not in producoes_indeterminantes.items():
+            new_grammar[nonterminal] = productions
+
+    repeated_terminals = {}
+    for nonterminal, productions in producoes_indeterminantes.items():
+        for terminal, *tail in productions:
+            # S', S'', S'''...
+            suffix = "'" * (len(repeated_terminals) + 1)
+            name = repeated_terminals.get(terminal, f"{nonterminal}{suffix}")
+            repeated_terminals[terminal] = name
+            # S' -> A | B | &
+            try:
+                new_grammar[name] += [tail]
+            except KeyError:
+                new_grammar[name] = [tail]
+            # S -> aS' | bS''
+            new_production = [terminal, fr'\{name}']
+            try:
+                if new_production not in new_grammar[nonterminal]:
+                    new_grammar[nonterminal] += [new_production]
+            except KeyError:
+                new_grammar[nonterminal] = [new_production]
+
+    return new_grammar
+
+def getFirst(grammar: dict):
+    first = {}
+    adicionei = True
+    while adicionei:
+        print(f'            rodando while')
+        adicionei = False
+        for cabeca in grammar:
+            for producao in grammar[cabeca]:
+                # epsilon
+                if producao == ['epsilon']:
+                    first, inserido = dictExclusiveInsertAppend(first, cabeca, producao)
+                    adicionei = (adicionei | inserido)
+                    print(f'adicionando epsilon a first de {cabeca}')
+                    # print('continue')
+                    continue
+
+                # terminal
+                if type(producao) == str:
+                    first, inserido = dictExclusiveInsertAppend(first, cabeca, producao)
+                    adicionei = (adicionei | inserido)
+                    print(f'adicionando terminal sozinho {producao} a first de {cabeca}')
+                    # print('continue')
+                    continue
+
+                # terminal
+                if type(producao[0]) == str:
+                    first, inserido = dictExclusiveInsertAppend(first, cabeca, producao[0])
+                    adicionei = (adicionei | inserido)
+                    print(f'adicionando terminal {producao[0]} em first de {cabeca}')
+                    # print('continue')
+                    continue
+                print(f'passei reto com {producao}')
+
+                # n terminal
+                if type(producao[0]) == list:
+                    if producao[0][0] in first:
+                        print(f'a producao que eu to olhando é {producao[0][0]}, cabeca {cabeca}')
+                        print(first)
+                        espandir = False
+                        tryExtend = copy.deepcopy(first)
+                        if cabeca not in tryExtend:
+                            tryExtend[cabeca] = first[producao[0][0][0]]
+                        else:
+                            tryExtend[cabeca].extend(first[producao[0][0][0]])
+                        print(tryExtend)
+                        print(first)
+                        first, inserido = dictExclusiveInsertAppend(first, cabeca, first[producao[0][0]])
+                        adicionei = (adicionei | inserido)
+                        print(f'expandindo por nao terminal {producao[0]} em first de {cabeca} com {first[producao[0][0]]}')
+                        # print('continue')
+                        continue
+                print(f'passei reto com {producao}')
+
+    for cabeca in grammar:
+        for producao in grammar[cabeca]:
+            # nterminal
+            for x in producao[0]:
+                pass
+    for x in first:
+        print(f'first[{x}] = {first[x]}')
+    return first
+
+
+
+
+def fatoracao(grammar: dict):
+    if len(direct_indeterminant_productions(grammar)) > 0:
+        grammar = remove_direct_indetermination(grammar)
+    getFirst(grammar)
+        # for x in ndd:
+        #     print(f'{x}->{ndd[x]}')
+    # print(f'ndd {ndd}')
+    return grammar
+
+
+
+if __name__ == '__main__':
+    print('Usage: don\'t')
--- test_analisador_sintatico.py
+++ test_analisador_sintatico.py
@@ -1,14 +1,21 @@
-from analisador_sintatico import remove_left_recursion
+from analisador_sintatico import (
+    remove_left_recursion,
+    fatoracao,
+    remove_direct_indetermination,
+    direct_indeterminant_productions,
+    )
 
-# S -> Sa | Ab
-# A -> Sb | b
 
-# S -> AbS'
-# S'-> aS' | epsilon
-# A -> bA'
-# A'-> bS'bA' | epsilon
 
 def test_remove_left_recursion():
+    # S -> Sa | Ab
+    # A -> Sb | b
+
+    # S -> AbS'
+    # S'-> aS' | epsilon
+    # A -> bA'
+    # A'-> bS'bA' | epsilon
+
     grammar = {
         "S": [[r"\S", "a"], [r"\A", "b"]],
         "A": [[r"\S","b"], ["b"]]
@@ -20,3 +27,193 @@ def test_remove_left_recursion():
         "A'": [['b', r"\S'", 'b', r"\A'"], ['epsilon']]
     }
     assert remove_left_recursion(grammar) == expected
+
+def test_fatoracao():
+
+    # S -> AC | BC
+    # A -> aD | cC
+    # B -> aB | dD
+    # C -> eC | eA
+    # D -> fD | CB
+
+    # S -> AS'| cCC | dDC
+    # S'-> DC | BC
+    # A -> aD | cC
+    # B -> aB | dD
+    # C -> eC'
+    # C'-> C | A
+    # D -> fD | CB
+
+    grammar = {
+        "S": [[r"\A", r"\C"], [r"\B", r"\C"]],
+        "A": [["a", r"\D"], ["c", r"\C"]],
+        "B": [["a", r"\B"], ["d", r"\D"]],
+        "C": [["e", r"\C"], ["e", r"\A"]],
+        "D": [["f", r"\D"], [r"\C", r"\B"]],
+
+    }
+    expected = {
+        "S": [[r"\A", r"\S'"], ["c", r"\C", r"\C"], ["d", r"\D", r"\C"]],
+        "S'":[[r"\D", r"\C"], [r"\B", r"\C"]],
+        "A": [["a", r"\D"], ["c", r"\C"]],
+        "B": [["a", r"\B"], ["d", r"\D"]],
+        "C": [["e", r"\C'"]],
+        "C'":[[r"\C"], [r"\A"]],
+        "D": [["f", r"\D"], [r"\C", r"\B"]],
+    }
+
+    # assert fatoracao(grammar) == expected
+
+
+def test_do_seis():
+    grammar = {
+        "S":[
+            [r'\A', 'b'],
+            [r'\A', r'\B','c']
+            ],
+        "B":[
+            ['b', r'\B'],
+            [r'\A','d'],
+            ['epsilon'],
+            ],
+        "A":[
+            [r'\A','a'],
+            ['epsilon']
+            ],
+    }
+
+def test_producoes_indeterminantes():
+    grammar = {
+        'S':[
+            ['a', r'\A'],
+            ['a', r'\B'],
+            ['a'],
+        ],
+        'A':[
+            ['c'],
+        ],
+        'B':[
+            ['d'],
+        ],
+    }
+    expected = {
+        'S': [
+            ['a',r'\A'],
+            ['a',r'\B'],
+            ['a'],
+        ],
+    }
+
+    assert direct_indeterminant_productions(grammar) == expected
+
+
+def test_producoes_indeterminantes_multiple_from_same_nonterminal():
+    grammar = {
+        'S':[
+            ['a', r'\A'],
+            ['a', r'\B'],
+            ['a'],
+            ['b', r'\A'],
+            ['b', r'\B'],
+        ],
+        'A':[
+            ['c'],
+        ],
+        'B':[
+            ['d'],
+        ],
+    }
+    expected = {
+        'S': [
+            ['a',r'\A'],
+            ['a',r'\B'],
+            ['a'],
+            ['b', r'\A'],
+            ['b', r'\B'],
+        ],
+    }
+
+    assert direct_indeterminant_productions(grammar) == expected
+
+
+def test_remove_direct_indetermination_simple():
+    grammar = {
+        'S':[
+            ['a',r'\A'],
+            ['a',r'\B']
+        ],
+        'A':[
+            ['c'],
+        ],
+        'B':[
+            ['d'],
+        ],
+    }
+    expected = {
+        'S':[
+            ['a',r"\S'"],
+        ],
+        "S'":[
+            [r"\A"],
+            [r"\B"]
+        ],
+        'A':[
+            ['c'],
+        ],
+        'B':[
+            ['d'],
+        ],
+    }
+
+    assert remove_direct_indetermination(grammar) == expected
+
+
+def test_remove_direct_indetermination_multiple_on_same_nonterminal():
+    grammar = {
+        'S':[
+            ['a', r'\A'],
+            ['a', r'\B'],
+            ['b', r'\C'],
+            ['b', r'\D'],
+        ],
+        'A':[
+            ['c'],
+        ],
+        'B':[
+            ['d'],
+        ],
+        'C':[
+            ['e'],
+        ],
+        'D':[
+            ['f'],
+        ],
+    }
+    expected = {
+        'S':[
+            ['a', r"\S'"],
+            ['b', r"\S''"],
+        ],
+        "S'":[
+            [r"\A"],
+            [r"\B"],
+        ],
+        "S''":[
+            [r"\C"],
+            [r"\D"],
+        ],
+        'A':[
+            ['c'],
+        ],
+        'B':[
+            ['d'],
+        ],
+        'C':[
+            ['e'],
+        ],
+        'D':[
+            ['f'],
+        ],
+    }
+
+    assert remove_direct_indetermination(grammar) == expected
